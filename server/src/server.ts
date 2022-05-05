6abe1bebc56e7cbf53574b60cf78193c283929b9
rcapeto <raphaelcapeto@gmail.com>
2022-05-05 13:48:39 -0300

    end mobile development and change server request limit

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ import { route } from './routes/feedback';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 
 app.use('/feedbacks', route);
 
